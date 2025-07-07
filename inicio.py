f73047c9306d07e6c7215166894f17c9e6d2d466
profe Luis Garrido <profe.lgarridocornejo@gmail.com>
2025-07-07 18:09:08 -0400

    20250707

---
+++ inicio.py
@@ -0,0 +1,34 @@
+
+import streamlit as st
+
+
+
+intro = """🎯 **Introducción a la Aplicación Interactiva en Streamlit**
+
+Con estos ejercicios hemos desarrollado una serie de aplicaciones interactivas utilizando Streamlit, una herramienta que nos permite construir interfaces visuales rápidas y efectivas en Python. Cada ejercicio tiene como objetivo explorar y visualizar datos reales, fomentando la comprensión de análisis exploratorio de datos (EDA) y fortaleciendo habilidades de visualización geográfica y estadística. A continuación, se describen los tres ejercicios principales desarrollados:"""
+
+capitales = """1. 🌍 **Capitales del Mundo**
+
+Este ejercicio permite al usuario seleccionar un continente y país, tras lo cual se muestra la capital correspondiente junto con un mapa interactivo que localiza su ubicación geográfica. Es una forma simple y visual de explorar la geografía política del mundo y familiarizarse con la ubicación de distintas capitales."""
+
+pinguinos = """2. 🐧 **Especies de Pingüinos**
+
+En este ejercicio se trabaja con un conjunto de datos de la Isla Palmer (Antártida), donde se puede elegir una especie de pingüino. Se genera automáticamente un gráfico de dispersión que relaciona la longitud del pico (culmen) con la longitud de la aleta (flipper). Además, se destaca visualmente la diferencia entre sexos, permitiendo observar patrones o diferencias morfológicas entre hembras y machos."""
+
+terremotos = """3. 🌐 **Terremotos Históricos**
+
+En esta sección se analizan el TOP de los terremotos más importantes registrados. El usuario puede elegir entre visualizar:
+
+- El terremoto de mayor magnitud,
+
+- El de mayor daño económico,
+
+- El de mayores pérdidas humanas.
+
+Luego, se selecciona cuántos registros visualizar (del top 1 al 100), y se muestra un mapa georreferenciado con los eventos seleccionados. Esto permite comprender mejor la distribución y el impacto global de los grandes sismos."""
+
+
+st.markdown(intro)
+st.markdown(capitales)
+st.markdown(pinguinos)
+st.markdown(terremotos)
